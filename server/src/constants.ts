f2cc452bcf5035d925eac893ffbc56da808f4ff9
admin <manhduong240101@gmail.com>
2021-11-13 20:22:38 +0700

    add client and server

---
+++ server/src/constants.ts
@@ -0,0 +1,2 @@
+export const COOKIE_NAME: string = "reddit-cookie"
+export const __prod__: boolean = process.env.NODE_ENV === 'production'
---
+++ server/src/types/UserMutationResponse.ts
@@ -0,0 +1,17 @@
+import { Field, ObjectType } from "type-graphql";
+import { User } from "../../entities/User";
+import { FieldError } from "./FieldError";
+import { IMutationResponse } from "./MutationResponse";
+
+@ObjectType({ implements: IMutationResponse })
+export class UserMutationResponse implements IMutationResponse {
+  code!: number;
+  success!: boolean;
+  message?: string;
+
+  @Field({ nullable: true })
+  user?: User;
+
+  @Field((_type) => [FieldError], { nullable: true })
+  errors?: FieldError[];
+}
